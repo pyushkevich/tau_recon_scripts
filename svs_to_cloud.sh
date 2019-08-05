26e24728f6574220bbc21133e17410097e0239c6
Paul Yushkevich <pyushkevich@gmail.com>
2019-08-05 13:24:56 -0400

    started adding the preproc loop

--- svs_to_cloud.sh
+++ svs_to_cloud.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -x -e
 
+# ----------------------------------------------------
+# Scripts to perform preprocessing of histology on GCP
+# ----------------------------------------------------
+
 # Globals
 ROOT=/data/picsl/pauly/tau_atlas
 MDIR=$ROOT/manifest
 
-# Simple script: go through all of the Google spreadsheets and for each one, 
-# find the SVS files that are 'usable' and upload them to the cloud
-function process_specimen()
+# Get all the slide identifiers in the manifest file for a specimen
+function get_specimen_manifest_slides()
 {
   # What specimen and block are we doing this for?
   read -r id args <<< "$@"
@@ -20,15 +23,26 @@ function process_specimen()
   fi
 
   # Read the relevant slides in the manifest
-  SVSLIST=$(curl -s "$url" 2>&1 | \
+  curl -s "$url" 2>&1 | \
     grep -v duplicate | \
     grep -v multiple | \
-    awk -F, 'NR > 1 {print $1}')
+    awk -F, 'NR > 1 {print $1}'
+}
+
+# Simple script: go through all of the Google spreadsheets and for each one, 
+# find the SVS files that are 'usable' and upload them to the cloud
+function upload_to_bucket_specimen()
+{
+  # What specimen and block are we doing this for?
+  read -r id args <<< "$@"
+
+  # Match the id in the manifest
+  SVSLIST=$(get_specimen_manifest_slides $id)
 
   # Launch array tasks
   export SVSLIST id
   qsub -sync y -j y -o $ROOT/dump -cwd -V -t 1-$(echo $SVSLIST | wc -w) -tc 6 \
-    $0 process_slide_task 
+    $0 upload_to_bucket_slide_task 
 }
 
 # Get a complete URL to the file
@@ -41,7 +55,7 @@ function get_slide_list_single()
 }
 
 # Process an individual slide task
-function process_slide()
+function upload_to_bucket_slide()
 {
   read -r id svs args <<< "$@"
 
@@ -55,7 +69,7 @@ function process_slide()
   # Form a URL for the destination
   dst_url="gs://mtl_histology/HNL-11-15/histo_raw/${svs}.${ext}"
 
-  echo "process_slide $id $svs $src_url $dst_url"
+  echo "upload_to_bucket_slide $id $svs $src_url $dst_url"
 
 
   # Check if the file exists at destination
@@ -68,20 +82,30 @@ function process_slide()
 }
 
 # Process an individual task
-function process_slide_task()
+function upload_to_bucket_slide_task()
 {
   SVS=$(echo $SVSLIST | cut -d " " -f $SGE_TASK_ID)
 
   # Locate the slide on the histology drive
-  process_slide $id $SVS
+  upload_to_bucket_slide $id $SVS
 }
 
 # main
-function main()
+function upload_to_bucket_all()
 {
   for id in $(cat $MDIR/histo_matching.txt | awk '{print $1}'); do
-    process_specimen $id
+    upload_to_bucket_specimen $id
   done
+}
+
+
+# List all raw images that are in manifest files and exist on the remote server
+function get_specimen_cloud_raw_slides()
+{
+  # Launch array tasks
+  export SVSLIST id
+  qsub -sync y -j y -o $ROOT/dump -cwd -V -t 1-$(echo $SVSLIST | wc -w) -tc 6 \
+    $0 upload_to_bucket_slide_task 
 }
 
 # Main entrypoint into script
